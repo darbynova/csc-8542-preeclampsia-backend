d852ce12d2c4a3fe8d5efbeacaf137624d9edb38
darbynova <cdarby01@villanova.edu>
2024-11-12 15:33:23 -0500

    init

---
+++ backend/preeclampsia/src/main/java/edu/villanova/csc8542/preeclampsia/PreeclampsiaApplication.java
@@ -0,0 +1,13 @@
+package edu.villanova.csc8542.preeclampsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreeclampsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreeclampsiaApplication.class, args);
+	}
+
+}
