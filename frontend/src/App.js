961bfc906c579e2b8bdb64b6903d241cc2573eaa
Adria Retter <aretter@Adrias-MacBook-Air.local>
2024-11-13 19:34:42 -0500

    readings page with dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
 function App() {
+  
+  const exampleData = { 
+    "BPDataList": [
+      {
+        "HP": 120,
+        "HR": 80,
+        "LP": 80,
+      }
+    ]
+  }
+  const [data, setData] = useState({ HR: '', LP: '', HP: '' });
+
+  // using exampleData for testing
+  useEffect(() => {
+    setData(exampleData['BPDataList'][0]);
+  }, []);
+  
+  const [loading, setLoading] = useState(true);
+
+  // simulating loading time to see the spinner
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+
+
+/*
+  useEffect(() => {
+    setLoading(true);
+    axios.get('/api/data') //put our actual api endpoint here
+      .then(response => {
+        setData(response.data['BPDataList'][0]);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
+  }, []);
+    */
+if (loading) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Measuring </h1>
+        <Loading />
+      </header>
+    </div>
+  );
+}
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Heart Rate: {data.HR} </h1> 
+        <h2> Blood Pressure: {data.HP} / {data.LP} </h2>
       </header>
     </div>
   );
